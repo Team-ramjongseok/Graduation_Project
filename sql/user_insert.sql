a9f9d6ce5b821ec57e05ad13dfebb26dc3fcc697
jonghyun <jhbaik1501@naver.com>
2022-04-12 17:28:48 +0900

    [feat] user_insert

--- sql/user_insert.sql
+++ sql/user_insert.sql
@@ -6,4 +6,4 @@ values ('jonghyun', 'a@naver.com', '010-111-1111', 'qwer');
 
 -- 카페 insert문
 insert into cafes (name, cafe_img, cafe_info, operation, location, seat_empty, seat_all)
-values ('starbucks','123', '스타벅스입니다.', 0, 12, 3, 30);
+values ('starbucks','123', '스타벅스입니다.', '00:00-08:00', '서울', 3, 30);
