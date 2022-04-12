fdea1b6fd797ed5cb8877bb0c0c724e4c0637192
jonghyun <jhbaik1501@gmail.com>
2022-04-12 19:59:42 +0900

    main.js change

--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 const User = require('../models').User;
-const {findUser, hi} = require('../repository/user');
+const userRepository = require('../repository/user');
 
 router.get('/', async (req, res)=> {
-    const a = await findUser();
+    const a = await userRepository.findUser();
     console.log("==============\n a:", a);
     res.send(a);
 });
 
 router.get('/cafes', async (req, res)=> {
-    const b = await hi();
+    const b = await userRepository.hi();
     console.log("==============\n a:", b);
     res.send(b);
 });
