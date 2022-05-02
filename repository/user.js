0d9575d996e60fb88179c6352a7bece34730b65d
wonseok <tkddls4456@gmail.com>
2022-05-03 02:05:44 +0900

    [feat] req_user&res_cafes

--- repository/user.js
+++ repository/user.js
@@ -4,6 +4,21 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
+function distanceCal(user,cafes) {
+    //CAFE 위,경도 NULL시 처리 해줘야함
+    for (let cafe of cafes){ // 거리구하기
+        let x = (user[0].latitude - cafe.latitude) *100000.0 *0.884;
+        let y = (user[0].longitude - cafe.longitude) *100000.0 *1.110;
+        cafe.distance = Math.sqrt((x*x)+(y*y));
+    }
+
+    const distanceResult = cafes.sort((a,b)=> { //거리순 정렬
+        return a.distance - b.distance;
+    })
+
+    return distanceResult;
+}
+
 // 유저를 찾는
 const findUser = async (username)=>{
     const [result, meatadata] = await sequelize.query('SELECT nickname,latitude,longitude  FROM USERS');
@@ -24,23 +39,25 @@ const findCafes = async ()=> {
     return result;
 }
 
+// 위,경도 삽입
+const updateUser = async (latitude,longitude)=> {
+
+    let sql = (`UPDATE USERS SET latitude=${latitude}, longitude=${longitude} WHERE id=1;`);
+    await sequelize.query(sql);
+    sql = ("SELECT * from USERS WHERE id= 1;");
+    const [result, meatadata] = await sequelize.query(sql)
+    return result;
+}
+// cafe 가까운순으로 return
 const nearCafes = async (user,cafes)=> {
-    let distanceResult;
-
-    for (let cafe of cafes){ // 거리구하기
-        let x = (user[0].latitude - cafe.latitude) *100000.0 *0.884;
-        let y = (user[0].longitude - cafe.longitude) *100000.0 *1.110;
-        cafe.distance = Math.sqrt((x*x)+(y*y));
-    }
-
-    distanceResult = cafes.sort((a,b)=> { //거리순 정렬
-        return a.distance - b.distance;
-    })
+    const distanceResult = await distanceCal(user,cafes);
     return distanceResult
 }
+
 
 
 exports.findUser = findUser;
 exports.findCafes = findCafes;
 exports.findMyProfile = findMyProfile;
 exports.nearCafes = nearCafes;
+exports.updateUser = updateUser;
--- routes/main.js
+++ routes/main.js
@@ -3,20 +3,38 @@ const User = require('../models').User;
 const userRepository = require('../repository/user');
 
 router.get('/', async (req, res)=> {
-    const cafes = await userRepository.findCafes();
-    let {nickname} = req.query;
     
-    if (nickname){ // query parameter로 nickname 입력시
-        const my_profile = await userRepository.findMyProfile(nickname);
-        const distanceResult =  await userRepository.nearCafes(my_profile,cafes);
 
-        res.send({'내정보':my_profile, '가까운순':distanceResult});
-    }
-    else{ //기본 main
+     //기본 main
+        const cafes = await userRepository.findCafes();
         const cafe_user = await userRepository.findUser();
         const result = {cafe_user,cafes};
         res.send(result);
-    }
+    
+});
+
+router.post('/gps', async(req,res)=>{
+    const {latitude, longitude,nickname} = req.body;
+    const my_profile = await userRepository.updateUser(latitude, longitude);
+    const cafes = await userRepository.findCafes();
+    const distanceResult =  await userRepository.nearCafes(my_profile,cafes);
+
+    console.log(distanceResult);
+    const latitude_li = distanceResult.reduce((prev,cur)=> {
+        prev.push(cur.latitude);
+        return prev;
+    },[])
+
+    const longitude_li = distanceResult.reduce((prev,cur)=> {
+        prev.push(cur.longitude);
+        return prev;
+    },[])
+
+    res.json([{
+        latitude: latitude_li,
+        longitude: longitude_li,
+    
+    }]);
 });
 
 module.exports = router;
